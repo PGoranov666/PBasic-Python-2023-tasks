a10531999d1fe1e5e9f297126e036f5b233e2e14
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-09-24 14:37:39 +0100

    Add files via upload

---
+++ 04_For Loop _Lab/04_Numbers_N...1.py
@@ -0,0 +1,4 @@
+number = int(input())
+
+for count in range(number, 0, -1):
+    print(count)
---
+++ 04_For Loop _Lab/07_Sum_Numbers.py
@@ -0,0 +1,7 @@
+number = int(input())
+sum = 0
+
+for multiplies in range(number):
+    num = int(input())
+    sum += num
+print(sum)
