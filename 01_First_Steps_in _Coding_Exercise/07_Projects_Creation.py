1a19cfe260ddb1d03ee375eede2223a77ef34aea
PGoranov666 <145895566+PGoranov666@users.noreply.github.com>
2023-09-24 14:34:01 +0100

    Add files via upload

---
+++ 01_First_Steps_in _Coding_Exercise/07_Projects_Creation.py
@@ -0,0 +1,4 @@
+architect_name = input()
+projects = int(input())
+project_time = projects * 3
+print(f"The architect {architect_name} will need {project_time} hours to complete {projects} project/s.")
